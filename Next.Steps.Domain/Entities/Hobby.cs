0ba813204db52fb8662c50fffcf8340e3f8b27ec
Pedro Mateus <pedromateus91@gmail.com>
2021-02-18 21:07:59 +0000

    Added Person Entity and Hobby Value Object to Domain

---
+++ Next.Steps.Domain/Entities/Hobby.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Next.Steps.Domain.Entities
+{
+    public class Hobby
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Next.Steps.Domain/Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Next.Steps.Domain.Entities
+{
+    public sealed class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Profession { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<Hobby> Hobbies { get; set; }
+    }
+}
