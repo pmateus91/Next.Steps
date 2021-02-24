b2840211f6bfcb90fbd271f362f80e886daf2b10
Pedro Mateus <pedromateus91@gmail.com>
2021-02-24 21:31:16 +0000

    Add BaseService + PersonService to Domain Services

---
+++ Next.Steps.Domain/Services/BaseService.cs
@@ -0,0 +1,78 @@
+﻿using Next.Steps.Domain.Interfaces.Repositories;
+using Next.Steps.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Next.Steps.Domain.Services
+{
+    public class BaseService<TEntity> : IServiceBase<TEntity> where TEntity : class
+    {
+        private IRepositoryBase<TEntity> _repo;
+
+        public BaseService(IRepositoryBase<TEntity> repo)
+        {
+            _repo = repo;
+        }
+
+        public bool Add(TEntity obj)
+        {
+            if (obj != null)
+            {
+                _repo.Add(obj);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _repo.GetById(id);
+        }
+
+        public bool Remove(int id)
+        {
+            if (id > 0)
+            {
+                _repo.Remove(id);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Update(TEntity obj)
+        {
+            if (obj != null)
+            {
+                _repo.Update(obj);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<TEntity> Search(string firstname, string lastname)
+        {
+            if (firstname == "" || lastname == "")
+            {
+                return null;
+            }
+            else
+            {
+                return _repo.Search(firstname, lastname);
+            }
+        }
+    }
+}
---
+++ Next.Steps.Domain/Services/PersonService.cs
@@ -0,0 +1,16 @@
+﻿using Next.Steps.Domain.Entities;
+using Next.Steps.Domain.Interfaces.Repositories;
+using Next.Steps.Domain.Interfaces.Services;
+
+namespace Next.Steps.Domain.Services
+{
+    public class PersonService : BaseService<Person>, IServicePerson
+    {
+        private IRepositoryPerson _repo;
+
+        public PersonService(IRepositoryPerson repo) : base(repo)
+        {
+            _repo = repo;
+        }
+    }
+}
