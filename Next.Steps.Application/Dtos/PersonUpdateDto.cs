454baa613835bf79a937bb69a9a21b8e36fea62a
Pedro Mateus <pedromateus91@gmail.com>
2021-02-25 17:08:02 +0000

    Add PersonUpdateDto

---
+++ Next.Steps.Application/Dtos/PersonUpdateDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Next.Steps.Application.Dtos
+{
+    public class PersonUpdateDto
+    {
+        [Required(ErrorMessage = "Person's Id is Required")]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Profession { get; set; }
+
+        public DateTime? Birthdate { get; set; }
+
+        public string Email { get; set; }
+
+        public List<HobbyDto> Hobbies { get; set; }
+    }
+}
