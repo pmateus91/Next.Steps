7c65a5728f034959161695c355a09ee34841bda2
Pedro Mateus <pedromateus91@gmail.com>
2021-02-25 22:03:11 +0000

    Change all interface in BaseRepository class to virutal

--- Next.Steps.Repository/Repository/BaseRepository.cs
+++ Next.Steps.Repository/Repository/BaseRepository.cs
@@ -16,31 +16,31 @@ namespace Next.Steps.Infrastructure.Repository
             this._nextStepsContext = nextStepsContext;
         }
 
-        public IEnumerable<TEntity> GetAll()
+        public virtual IEnumerable<TEntity> GetAll()
         {
             return _nextStepsContext.Set<TEntity>().ToList();
         }
 
-        public TEntity GetById(int id)
+        public virtual TEntity GetById(int id)
         {
             return _nextStepsContext.Set<TEntity>().Find(id);
         }
 
-        public bool Add(TEntity obj)
+        public virtual bool Add(TEntity obj)
         {
             _nextStepsContext.Set<TEntity>().Add(obj);
             _nextStepsContext.SaveChanges();
             return true;
         }
 
-        public bool Update(TEntity obj)
+        public virtual bool Update(TEntity obj)
         {
             _nextStepsContext.Entry(obj).State = EntityState.Modified;
             _nextStepsContext.SaveChanges();
             return true;
         }
 
-        public bool Remove(TEntity obj)
+        public virtual bool Remove(TEntity obj)
         {
             _nextStepsContext.Set<TEntity>().Remove(obj);
             _nextStepsContext.SaveChanges();
