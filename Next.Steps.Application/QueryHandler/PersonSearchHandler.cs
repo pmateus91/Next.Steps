382f297939847b1a1e231660881dd0675f7c29e8
Pedro Mateus <pedromateus91@gmail.com>
2021-02-26 00:57:48 +0000

    QuerySearchHandler Implemention

--- Next.Steps.Application/QueryHandler/PersonSearchHandler.cs
+++ Next.Steps.Application/QueryHandler/PersonSearchHandler.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using AutoMapper;
+using MediatR;
+using Next.Steps.Application.Dtos;
+using Next.Steps.Application.Query;
+using Next.Steps.Domain.Entities;
+using Next.Steps.Domain.Interfaces.Services;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Next.Steps.Application.QueryHandler
 {
-    public class PersonSearchHandler
+    public class PersonSearchHandler : RequestHandler<PersonSearchQuery, IEnumerable<PersonReadDto>>
     {
+        private readonly IServicePerson _servicePerson;
+        private readonly IMapper _mapper;
+
+        public PersonSearchHandler(IServicePerson servicePerson, IMapper mapper)
+        {
+            _servicePerson = servicePerson;
+            _mapper = mapper;
+        }
+
+        protected override IEnumerable<PersonReadDto> Handle(PersonSearchQuery request)
+        {
+            var person = _servicePerson.Search(request.Firstname, request.Lastname);
+
+            if (person != null)
+            {
+                return _mapper.Map<IEnumerable<Person>, IEnumerable<PersonReadDto>>(person);
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
